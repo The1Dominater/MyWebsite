db5254d13b9070a94e96bcbece53a1501547b05a
The1Dominater <galgano6@yahoo.com>
2025-02-06 00:39:45 -0700

    Udpated contact form

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+from dotenv import dotenv_values
+import secrets
 
 def create_app():
     app = Flask(__name__)
   
+    # Setup blueprint for different pages
     from .views import views
     app.register_blueprint(views, url_prefix='/')
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('error_400.html'), 404
+    
+    @app.errorhandler(401)
+    def unauthorized(e):
+        return render_template('error_401.html'), 401
+        
+    # Create a secret key for the contact form
+    app.secret_key = secrets.token_urlsafe(24)
+    print("secret key: ",app.secret_key)
+
+    # Setup reCAPTCHA secret keys
+    environment_vars = dotenv_values(".env")
+    app.config['RECAPTCHA_PUBLIC_KEY'] = environment_vars['SITE_KEY_V2']
+    app.config['RECAPTCHA_PRIVATE_KEY'] = environment_vars['SECRET_KEY_V2']
+    app.config['RECAPTCHA_PUBLIC_KEY_V3'] = environment_vars['SITE_KEY_V3']
+    app.config['RECAPTCHA_PRIVATE_KEY_V3'] = environment_vars['SECRET_KEY_V3']
+    app.config['VERIFY_URL'] = environment_vars['VERIFY_URL']
 
     return app
--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, current_app, render_template, request, abort, flash
+from .contact_form import ContactForm
+import requests
 
 views = Blueprint("views", __name__)
 
 @views.route('/')
 @views.route('/home')
-def aboutMe():
+def about_me():
     return render_template('2_about_me.html')
 
 @views.route('/resume')
@@ -12,9 +14,67 @@ def resume():
     return render_template('3_resume.html')
 
 @views.route('/my_projects')
-def myProjects():
+def my_projects():
     return render_template('4_my_projects.html')
 
-@views.route('/contact_info')
-def contactInfo():
-    return render_template('5_contact_info.html')
+def send_msg(sender_email:str, subject:str, message:str):
+    print(sender_email)
+    print(subject)
+    print(message)
+
+    return
+
+@views.route('/contact_info', methods=['GET','POST'])
+def contact_info():
+    form = ContactForm()
+    reCAPTCHA_site_key = current_app.config.get('RECAPTCHA_PUBLIC_KEY_V3')
+
+    if form.validate_on_submit():
+        # Honeypot fields
+        honeypot1 = form.name.data
+        honeypot2 = form.job_role.data
+        # print(honeypot1)
+        # print(honeypot2)
+
+        if (honeypot1 or honeypot2):
+            abort(401)
+
+        # Verify reCAPTCHA v3 score
+        reCAPTCHA_secret_key = current_app.config.get('RECAPTCHA_PRIVATE_KEY_V3')
+        verify_url = current_app.config.get('VERIFY_URL')
+        recaptcha_token = request.form.get("g-recaptcha-response")
+        print(recaptcha_token)
+        recaptcha_response = requests.post(
+            verify_url,
+            data={
+                "secret": reCAPTCHA_secret_key,
+                "response": recaptcha_token
+            }
+        ).json()
+
+        # Check if reCAPTCHA validation was successful
+        print(recaptcha_response)
+        if not (recaptcha_response.get("success") and recaptcha_response.get("score", 0) > 0.5):
+            flash("reCAPTCHA verification failed. Suspicous behavior detected.", category="error")
+            abort(401)
+            
+
+        # Necessary fields
+        sender_email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        if send_msg(sender_email,subject, message):
+            flash("Form submitted successfully!", category="success")
+            return render_template('6_sent_successfully.html')
+        else:
+           return render_template('7_send_failed.html') 
+    elif form.errors:
+        print(form.errors)
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash(f'Error with "{field}". {error}', category="error")
+    #     return render_template('7_send_failed.html')
+    
+    return render_template('5_contact_info.html', form=form, reCAPTCHA_site_key=reCAPTCHA_site_key)
+    
