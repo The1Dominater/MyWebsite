490b75b6200fd8cf200e1af0224e8c020b6530c7
The1Dominater <76396013+The1Dominater@users.noreply.github.com>
2025-10-18 00:13:46 -0600

    Reset git

--- website_static/static/index.js
+++ website_static/static/index.js
@@ -1,7 +1,3 @@
-/* 
-*  Yeah I know this whole this is a sin against web development,
-*  but its makes it so easy to host it as static content
-*/
 console.log("Loaded JS")
 
 // Allows for static website to use index.html as a template
@@ -32,6 +28,7 @@ window.onload = () => {
   loadPage(last);
 };
 
+<<<<<<< HEAD
 // Reset to home page if someone closes the tab
 var show_close_alert = true;
 $("a").bind("mouseup", function() {
@@ -47,9 +44,12 @@ $(window).bind("beforeunload", function() {
     }
 });
 
+=======
+>>>>>>> parent of 1c3efbf (Update resume download)
 // Load a list of the top 5 repos on my github
 const username = "The1Dominater";
 async function loadTopRepos(username, { sortBy = 'stars' } = {}) {
+    console.log("Fetching repos")
     const list = document.getElementById('repo-list');
     const url = `https://api.github.com/users/${encodeURIComponent(username)}/repos?type=owner&per_page=100&sort=updated`;
 
@@ -131,7 +131,11 @@ emailjs.init({
 function contactMe(e) {
   //e.preventDefault();
   
+<<<<<<< HEAD
   console.log("Sending email...")
+=======
+  console.log("Attempting to send email...")
+>>>>>>> parent of 1c3efbf (Update resume download)
   emailjs.sendForm('mywebsite-contactform', 'template_8ntibaq', e.target.form).then(
     (response) => {
       console.log('SUCCESS!', response.status, response.text);
